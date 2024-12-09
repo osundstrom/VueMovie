1e23eff62ad33f29a2cbba478037fdb5f7114170
osundstrom <oskar.sundstrom123@gmail.com>
2024-12-09 21:35:35 +0100

    update meny symbol

--- vue-project/src/components/headerNav.vue
+++ vue-project/src/components/headerNav.vue
@@ -20,19 +20,21 @@
 
   <div id="allContent">
   <div id="divHead">
-  <div class="dropdown">
+ 
+</div>
+</div>
 
-  <button class="dropdown btn btn-dark" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-    &#9776;
-  </button>
-  <ul class="dropdown-menu">
-    <li><RouterLink class="dropdown-item" to="/">Hem</RouterLink></li>
-    <li><RouterLink class="dropdown-item" to="/movie">Filmer</RouterLink></li>
-    <li><RouterLink class="dropdown-item" to="/vue">Vue</RouterLink></li>
-    
-  </ul>
-</div>
-</div>
+<div class="dropdown">
+
+<button class="dropdown btn btn-dark" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+  &#9776;
+</button>
+<ul class="dropdown-menu">
+  <li><RouterLink class="dropdown-item" to="/">Hem</RouterLink></li>
+  <li><RouterLink class="dropdown-item" to="/movie">Filmer</RouterLink></li>
+  <li><RouterLink class="dropdown-item" to="/vue">Vue</RouterLink></li>
+  
+</ul>
 </div>
 
 
@@ -48,14 +50,19 @@
     button{
       background-color: none  ;
       font-weight: 900;
-      font-size: x-large;
+      font-size: 120%;
       margin-top: 2vh;
       margin-left: 2vw;
       background:none;
       color: white;
       z-index: 1;
-
+      position: fixed;
+      top:-1vh;
+      left: -1vw;
+      
     }
+    
+
 
     ul{
       z-index: 1;
@@ -65,11 +72,9 @@
       background-image: url(../assets/greyAudience.jpg);
       background-size: cover;
       background-position: center;
-      height: 18vw;
+      height: 18vh;
       width: 100vw;
       position: relative;
-      top: 0;
-    left: 0;
     border-bottom: 1px solid black;
     z-index: 0;
     
