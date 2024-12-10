b127693d221d82cf6b2d2cc5c3ac63dc7e5095b7
osundstrom <oskar.sundstrom123@gmail.com>
2024-12-10 18:22:08 +0100

    gjort så man kan lägga till film

--- vue-project/src/components/addMovie.vue
+++ vue-project/src/components/addMovie.vue
@@ -3,14 +3,54 @@
         name: "addMovie",
     data() {
         return {
-            grade: 0
+            imdbRating: 0,
+            movie: {
+                name: "",
+                ratingImdb: 0,
+                watched: false,
+
+            }
+                
+            
         };
     },
+    emits:["movieAdded"],
     methods: {
         updateGrade(x) {
-            this.grade = x.target.value; 
+            this.ratingImdb = x.target.value; 
+        },
+        async addMovie(){
+            try {
+                
+
+                    let movieBody = this.movie
+                    console.log(movieBody)
+
+                    const res = await fetch("https://koamoment2.onrender.com/movie", {
+                        method:"POST",
+                        headers: {
+                                "Accept": "application/json",
+                                "Content-type": "application/json"
+                                },
+                        body: JSON.stringify(movieBody)
+                        })
+
+                    const data = await res.json();
+                    console.log(data)
+
+                    this.movie.ratingImdb = 0;
+                    this.movie.name = "";
+                    this.movie.watched = false;
+
+                    this.$emit("movieAdded")
+                    }
+
+            
+            catch(error) {
+                console.error(error)
+            }}
+        
         }
-    }
 };
 </script>
 
@@ -24,15 +64,15 @@
 <div style="min-height: 120px;">
   <div class="collapse collapse-horizontal" id="collapseWidthExample">
     <div class="card card-body" style="max-width: 80vw;">
-        <form>
+        <form @submit.prevent="addMovie">
         <label for="Inputname" class="form-label">Film titel</label>
-        <input type="text" class="form-control" id="Inputname" aria-describedby="name of movie">
-        <label for="customRange3" class="form-label">Betyg: {{ grade }}</label>
-        <input type="range" class="form-range" min="0" max="10" step="1" id="customRange3" :value="grade"  @input="updateGrade">
+        <input type="text" class="form-control" id="Inputname" aria-describedby="name of movie" v-model="movie.name">
+        <label for="customRange3" class="form-label">Betyg: {{ movie.ratingImdb }}</label>
+        <input type="range" class="form-range" min="0" max="10" step="1" id="customRange3" v-model="movie.ratingImdb" >
         <label class="form-check-label" for="flexSwitchCheckDefault"> Sett filmern:</label>
-        <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault">
+        <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" v-model="movie.watched">
         <hr>
-        <button type="button" class="btn btn-success btn-small">Lägg till</button>
+        <button type="submit" class="btn btn-success btn-small" >Lägg till</button>
     </form>
     </div>
   </div>
--- vue-project/src/views/movieView.vue
+++ vue-project/src/views/movieView.vue
@@ -56,7 +56,7 @@ export default {
 
 <template>
 <div>
-    <addMovie/>
+    <addMovie @movieAdded="getMovies()"/>
   </div>
 
   
