72bf7216a766fd963461f4e5b61b226b15702410
osundstrom <oskar.sundstrom123@gmail.com>
2025-08-17 22:25:01 +0200

    test commit

--- vue-project/src/views/HomeView.vue
+++ vue-project/src/views/HomeView.vue
@@ -16,7 +16,7 @@ export default {
  <!--Inehåll på sidna-->
   <main>
     <div>
-    <h1>Film</h1>
+    <h1>Film!</h1>
     <p>Vem gillar inte att titta på en bra film eller serie?</p>
     <p>Jag gillar att kolla på film och serier i alla fall och alla har vi våra favoriter. 
     Därav skapade jag en sida där man kan recensera filmer lite enkelt. (borde byggas på för att bli bättre så som text för att skriva åsikter, namn på den som lämnat recension osv). </p>
